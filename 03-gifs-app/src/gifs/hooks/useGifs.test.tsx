5766a7007f0953db5911892fd85ecbb54da0a636
Nicolas Rosales <nicolas.rosales@mi.unc.edu.ar>
2026-02-11 15:47:24 -0300

    update useGifs

--- 03-gifs-app/src/gifs/hooks/useGifs.test.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.test.tsx
@@ -9,13 +9,14 @@ vi.mock('../actions/get-gifs-by-query.action', () => ({
 
 
 describe('useGifs', () => {
-    const { result } = renderHook( () => useGifs() );
+    
+    // const { result } = renderHook( () => useGifs() );
     beforeEach(() => {
         vi.clearAllMocks();
     });
     
     test('should return default values and methods', () => {
-        
+        const { result } = renderHook( () => useGifs() );
         expect(result.current.gifs).toEqual([]);
         expect(result.current.handleSearch).toBeInstanceOf(Function);
         expect(result.current.handleSearch).toBeDefined();
@@ -24,7 +25,7 @@ describe('useGifs', () => {
         
     });
 
-    test('should return a list of gifs', () => {
+    test('should return a list of gifs', async () => {
         
         const mockGifs = Array.from({ length: 10 }, (_, i) => ({
             id: String(i + 1),
@@ -38,19 +39,19 @@ describe('useGifs', () => {
         
         const { result } = renderHook( () => useGifs() );
         
-        act(async () => {
-            await result.current.handleSearch("");
+        await act(async () => {
+            await result.current.handleSearch(" Goku ");
         });
 
-        waitFor(() => {
+        await waitFor(() => {
             expect(result.current.gifs).toHaveLength(10);
-            console.log(result.current.gifs);
-        }
-    )});
+        });
+        
+    });
        
     test('should return an empty list of gifs and not call API', async () => {
-        // const { result } = renderHook( () => useGifs() );
-        console.log(result.current)
+        const { result } = renderHook( () => useGifs() );
+
         await act(async () => {
             await result.current.handleSearch("");
             await result.current.handleSearch(" ");
@@ -59,5 +60,65 @@ describe('useGifs', () => {
         expect(result.current.previousTerms).toHaveLength(0);
         expect(result.current.gifs).toHaveLength(0);
     });
+
+    test('should return an empty list of gifs and not call API if the term is repeated', async () => {
+        const { result } = renderHook( () => useGifs() );
+        await act(async () => {
+            await result.current.handleSearch("Goku");
+        });
+        await waitFor(() => {
+            expect(result.current.previousTerms).toHaveLength(1);
+            expect(result.current.previousTerms[0]).toBe("goku");
+        });
+        await act(async () => {
+            await result.current.handleSearch("Goku");
+        });
+        expect(getGifsByQuery).toHaveBeenCalledTimes(1);
+        expect(result.current.previousTerms).toHaveLength(1);
+        expect(result.current.previousTerms[0]).toBe('goku');
+    });
+
+    test('should hold only 8 previous searches', async () => {
+        const { result } = renderHook( () => useGifs() );
+        const searchTerms = ["Goku", "One Piece", 
+            "Dragon Ball", "Bleach", "Attack on Titan", 
+            "My Hero Academia", "Demon Slayer", "Jujutsu Kaisen", "Naruto"];
+        for (const term of searchTerms) {
+            await act(async () => {
+                await result.current.handleSearch(term);
+            });
+        }
+        await waitFor(() => {
+            expect(result.current.previousTerms).toHaveLength(8);
+            expect(result.current.previousTerms[0]).toEqual('naruto');
+        });
+    });
+
+    test('should update gifs when a previous term is clicked', async () => {
+        const { result } = renderHook( () => useGifs() );   
+        await act(async () => {
+            await result.current.handleSearch("Goku");
+        });
+        
+        expect(getGifsByQuery).toHaveBeenCalledTimes(1);
+        expect(getGifsByQuery).toHaveBeenCalledWith("goku");
+
+        await waitFor(() => {
+            expect(result.current.gifs).toHaveLength(10);
+        });
+
+        await act(async () => {
+            await result.current.handleTermClicked("goku");
+        });
+
+        await waitFor(() => {
+            expect(result.current.gifs).toHaveLength(10);
+        }); 
+        expect(getGifsByQuery).toHaveBeenCalledTimes(1);
+
+
+    });
+
+
 
 });
